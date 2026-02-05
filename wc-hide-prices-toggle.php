d119cff021cf5db36767ebfb54cd57d6a4de86d2
Sajid Ashraf <sajidashraf.dev@gmail.com>
2026-02-05 21:46:01 +0500

    Implement WC_Hide_Prices_Toggle class for WooCommerce

--- wc-hide-prices-toggle.php
+++ wc-hide-prices-toggle.php
@@ -7,3 +7,30 @@
  * Requires at least: 6.0
  * Requires Plugins: woocommerce
  */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class WC_Hide_Prices_Toggle {
+	const OPTION_KEY = 'wchpt_settings';
+	const NONCE_KEY  = 'wchpt_bulk_action_nonce';
+
+	public function __construct() {
+		add_action( 'admin_menu', [ $this, 'add_settings_page' ] );
+		add_action( 'admin_init', [ $this, 'register_settings' ] );
+
+		// Frontend filters
+		add_filter( 'woocommerce_get_price_html', [ $this, 'maybe_hide_price_html' ], 999, 2 );
+		add_filter( 'woocommerce_short_description', [ $this, 'maybe_hide_short_description' ], 999 );
+
+		// Hide add to cart / purchasing
+		add_filter( 'woocommerce_is_purchasable', [ $this, 'maybe_make_not_purchasable' ], 999, 2 );
+		add_filter( 'woocommerce_variation_is_purchasable', [ $this, 'maybe_make_variation_not_purchasable' ], 999, 2 );
+
+		// Bulk action handlers
+		add_action( 'admin_post_wchpt_set_draft',   [ $this, 'handle_set_draft' ] );
+		add_action( 'admin_post_wchpt_set_publish', [ $this, 'handle_set_publish' ] );
+		add_action( 'admin_post_wchpt_set_price',   [ $this, 'handle_set_price' ] );
+
+		// Admin notice
+		add_action( 'admin_notices', [ $this, 'maybe_show_notice' ] );
+	}
